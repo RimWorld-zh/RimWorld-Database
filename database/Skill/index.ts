445440b37c86a43c3ad0d54cd8f855d61fd58305
duduluu <duduluu@outlook.com>
2018-01-07 22:49:15 +0800

    Update Model Skill: add aggregation

--- database/Skill/index.ts
+++ database/Skill/index.ts
@@ -26,4 +26,21 @@ const Skill: DatabaseSkill = {
   Intellectual,
 };
 
+export const aggregationSkill: AggrSkill = {
+  intervalTicks: 200,
+  minLevel: 0,
+  maxLevel: 20,
+  maxFullRateXpPerDay: 4000,
+  masterSkillThreshold: 14,
+  saturatedLearningFactor: 0.2,
+  learnFactorPassionNone: 0.35,
+  learnFactorPassionMinor: 1,
+  learnFactorPassionMajor: 1.5,
+  joyGainFactorPassionNone: 0,
+  joyGainFactorPassionMinor: 1.99999994947575E-05,
+  joyGainFactorPassionMajor: 3.9999998989515E-05,
+  xpRequirements: [1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 12000, 14000, 16000, 18000, 20000, 22000, 24000, 26000, 28000, 30000, 30000],
+  xpLosses: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -0.1, -0.2, -0.4, -0.6, -1, -1.8, -2.8, -4, -6, -8, -12]
+}
+
 export default Skill;
--- typings/aggr.d.ts
+++ typings/aggr.d.ts
@@ -1,5 +1,22 @@
 // Declaration for aggregation types.
 
+declare interface AggrSkill extends EAggr {
+  readonly intervalTicks: number;
+  readonly minLevel: number;
+  readonly maxLevel: number;
+  readonly maxFullRateXpPerDay: number;
+  readonly masterSkillThreshold: number;
+  readonly saturatedLearningFactor: number;
+  readonly learnFactorPassionNone: number;
+  readonly learnFactorPassionMinor: number;
+  readonly learnFactorPassionMajor: number;
+  readonly joyGainFactorPassionNone: number;
+  readonly joyGainFactorPassionMinor: number;
+  readonly joyGainFactorPassionMajor: number;
+  readonly xpRequirements: ReadonlyArray<number> | null;
+  readonly xpLosses: ReadonlyArray<number> | null;
+}
+
 declare interface AggrWorkType extends EAggr {
   readonly workTypsByPriority: ReadonlyArray<string | null> | null;
 }
